1940a84cac94c0e8d184b124f2057859a6dcdbd5
AugustoHFV <flores.vargas.augustoh@gmail.com>
2023-12-04 10:48:23 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var express = require('express');  
+var app = express();  
+var server = require('http').Server(app);  
+var io = require('socket.io')(server);
+
+var messages = [{  
+  id: 1,
+  text: "CHAT",
+}];
+
+app.use(express.static('public'));
+io.on('connection', function(socket) {  
+  console.log('Alguien se ha conectado ');
+  socket.emit('messages', messages);
+
+  socket.on('new-message', function(data) {
+    messages.push(data);
+
+    io.sockets.emit('messages', messages);
+  });
+});
+
+server.listen(8080, function() {  
+});
